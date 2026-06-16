79e167747bc97ee00e35c9b0d966053d7ace8c3a
LegoFan9 <97139608+Lego-Fan9@users.noreply.github.com>
2026-06-16 16:50:43 -0400

    Fix site-new build

--- site-new/src/ts/loc-bundle/brotli/wrapper.ts
+++ site-new/src/ts/loc-bundle/brotli/wrapper.ts
@@ -1,6 +1,12 @@
 // @ts-ignore
 import "./bjs.js";
 
+declare global {
+	interface Window {
+    BrotliDecompress(data: Int8Array): Uint8Array;
+  }
+}
+
 export default function decodeBrotli(arrayBuffer: ArrayBuffer): string {
   const br = new Int8Array(arrayBuffer);
   const decompressed = window.BrotliDecompress(br); // @ts-ignore
