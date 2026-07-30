3a18b4d1cbd12391c849a09d036b3fd5e8e4b1f2
Lego-Fan9 <curtis.chase18@gmail.com>
2026-07-29 22:55:32 -0400

    Add export

--- site-new/src/components/portrait-maker/export.tsx
+++ site-new/src/components/portrait-maker/export.tsx
@@ -1,19 +1,62 @@
 import styled from "styled-components";
+import { useState } from "react";
 
-//import { usePortraitMakerCtx } from "../../ts/portrait-maker/context.ts";
+import { usePortraitMakerCtx } from "../../ts/portrait-maker/context.ts";
 
 import { Card, CardRow } from "./card.tsx";
 
 export default function Export() {
+    const ctx = usePortraitMakerCtx();
+
+    const [displayMobileExport, setDisplayMobileExport] = useState(false);
+    const [mobileExportLink, setMobileExportLink] = useState("");
+
+    function genImage(): string | null {
+        const canvas = ctx.canvasRef.current;
+        if (!canvas) return null;
+
+        return canvas.toDataURL("image/webp", 0.9);
+    }
+
+    function exportPC() {
+        const imgUrl = genImage()
+        if (!imgUrl) return;
+
+        const a = document.createElement("a");
+        a.href = imgUrl;
+        a.download = "swgoh-portrait-maker.webp";
+        a.click();
+    }
+
+    function exportMobile() {
+        const imgUrl = genImage()
+        if (!imgUrl) return;
+
+        setMobileExportLink(imgUrl);
+        setDisplayMobileExport(true);
+    }
+
     return (
-        <Card>
-            <CardRow>
-                <Container>
-                    <ContainerItem className="common-button">Export for PC</ContainerItem>
-                    <ContainerItem className="common-button">Export for Mobile</ContainerItem>
-                </Container>
-            </CardRow>
-        </Card>
+        <>
+            <Card>
+                <CardRow>
+                    <Container>
+                        <ContainerItem className="common-button" onClick={() => exportPC()}>Export for PC</ContainerItem>
+                        <ContainerItem className="common-button" onClick={() => exportMobile()}>Export for Mobile</ContainerItem>
+                    </Container>
+                </CardRow>
+            </Card>
+            {displayMobileExport && (
+                <Card>
+                    <CardRow>
+                        <p style={{ color: "var(--text-dark)" }}>Tap and hold, then click Save Image</p>
+                    </CardRow>
+                    <CardRow>
+                        <img src={mobileExportLink} style={{ maxWidth: "100%" }} />
+                    </CardRow>
+                </Card>
+            )}
+        </>
     )
 }
 
