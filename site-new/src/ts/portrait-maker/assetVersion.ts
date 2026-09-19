deab30a215c1fd9bc85e80f0aad1a8e0be3fbce3
Lego-Fan9 <curtis.chase18@gmail.com>
2026-09-19 16:30:12 -0400

    Remove a wasteful api call that happened on page boot to get the assetVersion for portrait maker

--- site-new/src/ts/portrait-maker/assetVersion.ts
+++ site-new/src/ts/portrait-maker/assetVersion.ts
@@ -1,35 +1,33 @@
 interface MetaResponse {
-  data: {
-    assetVersion: string;
-  };
+	data: {
+		assetVersion: string;
+	};
 }
 
 let assetVersionPromise: Promise<string> | null = null;
 
 export async function GetAssetVersionGithub(): Promise<string> {
-  // This function is slightly stolen from swgoh-ae2
+	// This function is slightly stolen from swgoh-ae2 (which is C# lol)
 
-  if (!assetVersionPromise) {
-    assetVersionPromise = (async () => {
-      const versionGetterUrl =
-        "https://raw.githubusercontent.com/swgoh-utils/gamedata/refs/heads/main/meta.json";
+	if (!assetVersionPromise) {
+		assetVersionPromise = (async () => {
+			const versionGetterUrl =
+				"https://raw.githubusercontent.com/swgoh-utils/gamedata/refs/heads/main/meta.json";
 
-      const response = await fetch(versionGetterUrl);
+			const response = await fetch(versionGetterUrl);
 
-      if (!response.ok) {
-        console.error("Failed to get github version");
-        throw new Error("Failed to fetch asset version");
-      }
+			if (!response.ok) {
+				console.error("Failed to get github version");
+				throw new Error("Failed to fetch asset version");
+			}
 
-      const metaData = (await response.json()) as MetaResponse;
+			const metaData = (await response.json()) as MetaResponse;
 
-      console.log("Found AssetVersion:", metaData.data.assetVersion);
+			console.log("Found AssetVersion:", metaData.data.assetVersion);
 
-      return metaData.data.assetVersion;
-    })();
-  }
+			return metaData.data.assetVersion;
+		})();
+	}
 
-  return assetVersionPromise;
+	return assetVersionPromise;
 }
-
-await GetAssetVersionGithub();
